ce609ec0dd9d3af25ba5a39513642ca54dad3648
Cuongvl2003 <130202717+Cuongvl2003@users.noreply.github.com>
2024-05-26 23:27:41 +0700

    more

--- screens/Room.js
+++ screens/Room.js
@@ -8,6 +8,7 @@ import { MaterialCommunityIcons, SimpleLineIcons } from '@expo/vector-icons';
 import Checkbox from "expo-checkbox"
 import Button from '../components/Button';
 import { StatusBar } from 'expo-status-bar';
+import Slider from '@react-native-community/slider';
 
 
 
@@ -15,6 +16,7 @@ const Room = ({ navigation }) => {
 
     const [value, setValue] = useState(0);
     const [totalTaps, setTotalTaps] = useState(0);
+    const [speed, setSpeed] = useState(0);
 
     const incrementValue = () => {
         setValue(value + 1);
@@ -27,6 +29,19 @@ const Room = ({ navigation }) => {
     }
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
+    const increase = () => {
+        if (speed < 100){
+            const newSpeed = speed + 1;
+            setSpeed(newSpeed);
+        }
+    };
+    const decrease = () => {
+        if (speed > 0){
+            const newSpeed = speed - 1;
+            setSpeed(newSpeed);
+        }
+    };
 
     return(
         <LinearGradient
@@ -187,36 +202,70 @@ const Room = ({ navigation }) => {
                     <Text style={{color:'white',fontSize:20, marginLeft:50}}>Fan List:</Text>
                     <Text> </Text>
                     <ScrollView>
-                        <View style={styles.container1}> 
-                            <MaterialCommunityIcons name="lightbulb-on" size={24} color="#005ce6" />
-                            <Text style={{color:'black',fontSize:20}}>fan1</Text>
-                            <Switch
-                                trackColor={{false: '#c0bfc0', true: '#c0bfc0'}}
-                                thumbColor={isEnabled ? '#005ce6' : '#99989a'}
-                                ios_backgroundColor='#4dff88'
-                                onValueChange={toggleSwitch}
-                                value={isEnabled}
+                        <View style={styles.container4}>
+                            <View style={styles.container3}> 
+                                <MaterialCommunityIcons name="fan" size={24} color="#005ce6" />
+                                <Text style={{color:'black',fontSize:20}}>fan1</Text>
+                            
+                                <TouchableOpacity 
+                                    onPress={()=>navigation.navigate("DeviceSetting")}>
+                                    <Ionicons name="settings-outline" size={30} color="black" />
+                                </TouchableOpacity>
+                            
+                            </View>
+                            <Text style={{ 
+                                fontSize: 20,
+                                padding: 5,
+                                borderRadius: 10,
+                                fontWeight: 10,
+                                marginBottom: 15,
+                                }}>Speed: {speed}</Text>
+                            <Slider
+                                style = {{ width: 300, height: 20, transform: [{ scaleY: 2 }]}}
+                                value={speed}
+                                onValueChange={(value) => setSpeed(value)}
+                                minimumValue={0}
+                                maximumValue={100}
+                                step={1}
+                                minimumTrackTintColor="#005ce6"
+                                maximumTrackTintColor="#CCCCCC"
+                                thumbTintColor="#CCCCCC"
                             />
-                            <TouchableOpacity 
-                                onPress={()=>navigation.navigate("DeviceSetting")}>
-                                <Ionicons name="settings-outline" size={30} color="black" />
-                            </TouchableOpacity>
+                            <Text> </Text>
                         </View>
+
+
                         <Text> </Text>
-                        <View style={styles.container1}> 
-                            <MaterialCommunityIcons name="lightbulb-on" size={24} color="#005ce6" />
-                            <Text style={{color:'black',fontSize:20}}>fan1</Text>
-                            <Switch
-                                trackColor={{false: '#c0bfc0', true: '#c0bfc0'}}
-                                thumbColor={isEnabled ? '#005ce6' : '#99989a'}
-                                ios_backgroundColor='#4dff88'
-                                onValueChange={toggleSwitch}
-                                value={isEnabled}
+                        <View style={styles.container4}>
+                            <View style={styles.container3}> 
+                                <MaterialCommunityIcons name="fan" size={24} color="#005ce6" />
+                                <Text style={{color:'black',fontSize:20}}>fan2</Text>
+                            
+                                <TouchableOpacity 
+                                    onPress={()=>navigation.navigate("DeviceSetting")}>
+                                    <Ionicons name="settings-outline" size={30} color="black" />
+                                </TouchableOpacity>
+                            
+                            </View>
+                            <Text style={{ 
+                                fontSize: 20,
+                                padding: 5,
+                                borderRadius: 10,
+                                fontWeight: 10,
+                                marginBottom: 15,
+                                }}>Speed: {speed}</Text>
+                            <Slider
+                                style = {{ width: 300, height: 20, transform: [{ scaleY: 2 }]}}
+                                value={speed}
+                                onValueChange={(value) => setSpeed(value)}
+                                minimumValue={0}
+                                maximumValue={100}
+                                step={1}
+                                minimumTrackTintColor="#005ce6"
+                                maximumTrackTintColor="#CCCCCC"
+                                thumbTintColor="#CCCCCC"
                             />
-                            <TouchableOpacity 
-                                onPress={()=>navigation.navigate("DeviceSetting")}>
-                                <Ionicons name="settings-outline" size={30} color="black" />
-                            </TouchableOpacity>
+                            <Text> </Text>
                         </View>
                         <Text> </Text>
                         
@@ -282,12 +331,36 @@ const styles=StyleSheet.create({
     },
     container2:{
         flex: 1,
-
         borderWidth:2,
         borderColor:'white',
         borderRadius:20,
         marginLeft:10,
         marginRight:10,
         marginBottom:10
+    },
+    container3:{
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor:'white',
+        borderColor: 'white',
+        borderRadius:10,
+        borderWidth:10,
+        width:310,
+        marginLeft:30,
+        marginRight:30,
+    },
+    container4:{
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor:'white',
+        borderColor: 'white',
+        borderRadius:10,
+        borderWidth:2,
+        marginLeft:30,
+        marginRight:30
     },
 });
